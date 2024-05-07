f7cdc98a721860f0901da2dc09880de1a877b37e
Ahmed Fasih <wuzzyview@gmail.com>
2024-05-06 18:53:26 -0700

    prettier, wider lines because that was annoying me a surprising amount

---
+++ .prettierrc.mjs
@@ -0,0 +1,15 @@
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+  printWidth: 120,
+  proseWrap: "never",
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
